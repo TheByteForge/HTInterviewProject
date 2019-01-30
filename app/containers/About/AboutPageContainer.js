7a6bdca0cb58a4b8a59054b7a254a9d0ab3109a2
TheByteForge <zackwatkins@gmail.com>
2019-01-30 15:17:54 -0800

    added fake homepage image to about page

---
+++ app/containers/About/AboutPageContainer.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const AboutPageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #000;
+  img {
+    width: 100vw;
+    height: auto;
+  }
+`;
+
+export default AboutPageContainer;
--- app/containers/About/index.js
+++ app/containers/About/index.js
@@ -5,92 +5,87 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
-import styled, { keyframes } from 'styled-components';
 import makeSelectAbout from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import messages from './messages';
+// import messages from './messages';
 
 // Component Imports
 import Navigation from '../../components/Navigation';
 // import GoogleMaps from '../../components/GoogleMaps';
-import NotFoundPageContainer from '../NotFoundPage/NotFoundContainer';
+import AboutPageContainer from './AboutPageContainer';
 
-const slideInRight = keyframes`
-  from {
-    transform: translate3d(100%, 0, 0);
-    visibility: visible;
-  }
+// const slideInRight = keyframes`
+//   from {
+//     transform: translate3d(100%, 0, 0);
+//     visibility: visible;
+//   }
 
-  to {
-    transform: translate3d(0, 0, 0);
-  }
-`;
+//   to {
+//     transform: translate3d(0, 0, 0);
+//   }
+// `;
 
-const slideOutRight = keyframes`
-  from {
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-  }
+// const slideOutRight = keyframes`
+//   from {
+//     -webkit-transform: translate3d(0, 0, 0);
+//     transform: translate3d(0, 0, 0);
+//   }
 
-  to {
-    visibility: hidden;
-    -webkit-transform: translate3d(100%, 0, 0);
-    transform: translate3d(100%, 0, 0);
-  }
-`;
+//   to {
+//     visibility: hidden;
+//     -webkit-transform: translate3d(100%, 0, 0);
+//     transform: translate3d(100%, 0, 0);
+//   }
+// `;
 
-const AboutPageElm = styled.div`
-  background-color: #90caf9;
-  &.page-enter {
-    animation: ${slideInRight} 0.2s forwards;
-  }
-  &.page-exit {
-    animation: ${slideOutRight} 0.2s forwards;
-  }
-`;
+// const AboutPageElm = styled.div`
+//   background-color: #90caf9;
+//   &.page-enter {
+//     animation: ${slideInRight} 0.2s forwards;
+//   }
+//   &.page-exit {
+//     animation: ${slideOutRight} 0.2s forwards;
+//   }
+// `;
+
+import home from '../../images/Hotel-Rooms/HT-Home.jpg';
 
 /* eslint-disable react/prefer-stateless-function */
 export class About extends React.PureComponent {
   render() {
     return (
       <div>
-        <NotFoundPageContainer>
-          <Helmet>
-            <title>SignUp | Sign In</title>
-            <meta
-              name="description"
-              content="Description of Sign Up / Sign In Page"
-            />
-            <meta charset="utf-8" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-          </Helmet>
+        <Helmet>
+          <title>SignUp | Sign In</title>
+          <meta
+            name="description"
+            content="Description of Sign Up / Sign In Page"
+          />
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Helmet>
 
-          <Navigation />
-
-          <FormattedMessage {...messages.header} />
-
-          {/* <GoogleMaps /> */}
-        </NotFoundPageContainer>
-        <AboutPageElm />
+        <Navigation />
+        <AboutPageContainer>
+          <img src={home} alt="HotelTonightHome" />
+        </AboutPageContainer>
+        {/* <AboutPageElm /> */}
       </div>
     );
   }
 }
 
 About.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  // dispatch: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
